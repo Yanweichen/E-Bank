dbbd175a50d51a4e22b084e08bed08a530ecc29c
Yanweichen <yanweichen@live.com>
2016-03-23 15:19:23 +0800

    删除掉没用的import

--- src/main/java/com/bank/utils/JsonUtil.java
+++ src/main/java/com/bank/utils/JsonUtil.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.catalina.mbeans.UserMBean;
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
